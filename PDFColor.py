f9a03b7b59efbe3b7998237a9e0c1d5cabfad1b8
Mateusz <mati.2206@wp.pl>
2026-06-16 18:46:05 +0200

    refactor hotfix

--- PDFColor.py
+++ PDFColor.py
@@ -1,23 +1,21 @@
 import fitz  # PyMuPDF
 
-def colorRgbToPdf(color_rgb: tuple[float, float, float]) -> tuple:
-    r, g, b = color_rgb
-    return (f"{r} {g} {b} rh\n".encode(), f"{r} {g} {b} RG\n".encode())
-
 def colorText(doc: fitz.Document, color_rgb: tuple[float, float, float]):
     r, g, b = color_rgb
-    rgb_fill, rgb_stroke = colorRgbToPdf(color_rgb)
-
+    rgb_fill = f"{r} {g} {b} rg\n".encode()
+    rgb_stroke = f"{r} {g} {b} RG\n".encode()
+    
     for page in doc:
         for xref in page.get_contents():
             raw_code = doc.xref_stream(xref)
             new_code = rgb_fill + rgb_stroke + raw_code
             doc.update_stream(xref, new_code)
 
-def colorDrawing(doc: fitz.Document, color_rgb: tuple[float, float, float]):
+def colorDrawings(doc: fitz.Document, color_rgb: tuple[float, float, float]):
     r, g, b = color_rgb
-    rgb_fill, rgb_stroke = colorRgbToPdf(color_rgb)
-
+    rgb_fill = f"{r} {g} {b} rg\n".encode()
+    rgb_stroke = f"{r} {g} {b} RG\n".encode()
+    
     for page in doc:
         resources_list = page.get_xobjects()
         
@@ -31,14 +29,15 @@ def colorDrawing(doc: fitz.Document, color_rgb: tuple[float, float, float]):
             new_code = rgb_fill + rgb_stroke + new_code
             doc.update_stream(xref, new_code)
 
+
 def PDFColor(input_pdf: str, output_pdf: str, color_rgb: tuple[float, float, float]):
     doc = fitz.open(input_pdf)
 
     colorText(doc, color_rgb)
-    colorDrawing(doc, color_rgb)
+    colorDrawings(doc, color_rgb)
 
     doc.save(output_pdf)
     doc.close()
 
 # Run the script
-PDFColor("1a.pdf", "done.pdf", color_rgb=(1.0, 0.0, 1.0))
+PDFColor("1a.pdf", "done.pdf", color_rgb=(1, 0, 1))
