84b126e2bb42b9e9a0576d3310a959a2a26dd32b
Mateusz <mati.2206@wp.pl>
2026-06-16 18:35:14 +0200

    refactor

--- PDFColor.py
+++ PDFColor.py
@@ -1,19 +1,24 @@
 import fitz  # PyMuPDF
 
-def PDFColor(input_pdf, output_pdf, color_rgb):
-    doc = fitz.open(input_pdf)
+def colorRgbToPdf(color_rgb: tuple[float, float, float]) -> tuple:
     r, g, b = color_rgb
+    return (f"{r} {g} {b} rh\n".encode(), f"{r} {g} {b} RG\n".encode())
 
-    rgb_fill = f"{r} {g} {b} rg\n".encode()
-    rgb_stroke = f"{r} {g} {b} RG\n".encode()
-    
+def colorText(doc: fitz.Document, color_rgb: tuple[float, float, float]):
+    r, g, b = color_rgb
+    rgb_fill, rgb_stroke = colorRgbToPdf(color_rgb)
+
     for page in doc:
-
         for xref in page.get_contents():
             raw_code = doc.xref_stream(xref)
             new_code = rgb_fill + rgb_stroke + raw_code
             doc.update_stream(xref, new_code)
-            
+
+def colorDrawing(doc: fitz.Document, color_rgb: tuple[float, float, float]):
+    r, g, b = color_rgb
+    rgb_fill, rgb_stroke = colorRgbToPdf(color_rgb)
+
+    for page in doc:
         resources_list = page.get_xobjects()
         
         for resource in resources_list:
@@ -25,11 +30,15 @@ def PDFColor(input_pdf, output_pdf, color_rgb):
             
             new_code = rgb_fill + rgb_stroke + new_code
             doc.update_stream(xref, new_code)
-            
+
+def PDFColor(input_pdf: str, output_pdf: str, color_rgb: tuple[float, float, float]):
+    doc = fitz.open(input_pdf)
+
+    colorText(doc, color_rgb)
+    colorDrawing(doc, color_rgb)
 
     doc.save(output_pdf)
     doc.close()
-    print("Done! Deep label structures searched. Check the output file.")
 
 # Run the script
-PDFColor("1a.pdf", "done.pdf", color_rgb=(1, 0, 1))
+PDFColor("1a.pdf", "done.pdf", color_rgb=(1.0, 0.0, 1.0))
