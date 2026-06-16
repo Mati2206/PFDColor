d3ab85daa7bf80839c1bba202c8b4ec0d4aca746
Mateusz <mati.2206@wp.pl>
2026-06-16 20:59:00 +0200

    add image and barcode fix

--- PDFColor.py
+++ PDFColor.py
@@ -2,7 +2,6 @@ import os
 
 import fitz  # PyMuPDF
 from PIL import Image, ImageOps
-import numpy as np
 import io
 
 def rgb_to_pdf_color(rgb: tuple[float, float, float]) -> tuple[bytes, bytes]:
@@ -14,20 +13,78 @@ def color_text(doc: fitz.Document, color_rgb: tuple[float, float, float]):
     for page in doc:
         for xref in page.get_contents():
             raw_code = doc.xref_stream(xref)
-            new_code = rgb_fill + rgb_stroke + raw_code
+            new_code = raw_code.replace(b"\n0 G\n", b"\n" + rgb_stroke)
+            new_code = new_code.replace(b"\n0 g\n", b"\n" + rgb_fill)
+            new_code = new_code.replace(b" 0 G ", b" " + rgb_stroke.strip() + b" ")
+            new_code = new_code.replace(b" 0 g ", b" " + rgb_fill.strip() + b" ")
+            new_code = rgb_fill + rgb_stroke + new_code
             doc.update_stream(xref, new_code)
+
+# For specyfic pdf
+def color_shapes(doc: fitz.Document, color_rgb: tuple[float, float, float]):
+    for page in doc:
+        paths = page.get_drawings()
+
+        shape = fitz.Shape(page)
+
+        for path in paths:
+            stroke_opacity = path.get("stroke_opacity", 1.0)
+            fill_opacity = path.get("fill_opacity", 1.0)
+            if stroke_opacity is None: stroke_opacity = 1.0
+            if fill_opacity is None: fill_opacity = 1.0
+
+            color = color_rgb
+            
+            if path.get("fill") == (1.0, 1.0, 1.0) or path.get("color") == (1.0, 1.0, 1.0):
+                color = (1, 1, 1)
+
+            for item in path.get("items", []):
+                item_type = item[0]
+
+                if item_type == "re":
+                    rect = item[1]
+                    shape.draw_rect(rect)
+
+            width = path.get("width", 1.0)
+            if width is None:
+                width = 1.0
+
+            shape.finish(
+                color=color,
+                fill=color,
+                width=width,
+                stroke_opacity=stroke_opacity,
+                fill_opacity=fill_opacity
+            )
+
+        shape.commit()
 
 def color_drawings(doc: fitz.Document, color_rgb: tuple[float, float, float]):
     rgb_fill, rgb_stroke = rgb_to_pdf_color(color_rgb)
     for page in doc:
+        for xref in page.get_contents():
+            raw_code = doc.xref_stream(xref)
+            
+            new_code = raw_code
+            new_code = new_code.replace(b"\n0 G\n", b"\n" + rgb_stroke + b"\n")
+            new_code = new_code.replace(b"\n0 g\n", b"\n" + rgb_fill + b"\n")
+            
+            new_code = rgb_fill + rgb_stroke + new_code
+            doc.update_stream(xref, new_code)
+
         resources_list = page.get_xobjects()
         for resource in resources_list:
             xref = resource[0]
             raw_code = doc.xref_stream(xref)
             new_code = raw_code
+            
+            new_code = new_code.replace(b"\n0 G\n", b"\n" + rgb_stroke + b"\n")
             new_code = new_code.replace(b"\n0 g\n", b"\n" + rgb_fill + b"\n")
+            new_code = new_code.replace(b" 0 G ", b" " + rgb_stroke.strip() + b" ")
+            new_code = new_code.replace(b" 0 g ", b" " + rgb_fill.strip() + b" ")
             new_code = rgb_fill + rgb_stroke + new_code
             doc.update_stream(xref, new_code)
+    
 
 def color_images(doc: fitz.Document, color_rgb: tuple[float, float, float]):
     
@@ -57,6 +114,7 @@ def color_images(doc: fitz.Document, color_rgb: tuple[float, float, float]):
 def PDFColor(input_pdf: str, output_pdf: str, color_rgb: tuple[float, float, float]):
     doc = fitz.open(input_pdf)
 
+    color_shapes(doc, color_rgb)
     color_text(doc, color_rgb)
     color_drawings(doc, color_rgb)
     color_images(doc, color_rgb)
@@ -65,4 +123,4 @@ def PDFColor(input_pdf: str, output_pdf: str, color_rgb: tuple[float, float, flo
     doc.close()
 
 # Run the script
-PDFColor("2a.pdf", "done.pdf", color_rgb=(1, 0, 1))
+PDFColor("1a.pdf", "done.pdf", color_rgb=(1, 0, 1))
